61f6414b6d6e54b8770127f2cf2685d847800420
ChristopherHhh05 <91347773+ChristopherHhh05@users.noreply.github.com>
2024-11-06 15:19:11 -0500

    dataset initial setup updates...

--- docker-shell.sh
+++ docker-shell.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
+# Change to your USER NAME in line 24 if mounting .gitconfig in WSL to container
 
 set -e
 
 export BASE_DIR=$(pwd)
 export SECRETS_DIR=$(pwd)/../secrets/
-export GCS_BUCKET_NAME="cheese-app-data-versioning"
-export GCP_PROJECT="ac215-project"
-export GCP_ZONE="us-central1-a"
+export GCS_BUCKET_NAME="cheese-app-dvc-hw2"
+export GCP_PROJECT="angular-harmony-434717-n1"
+export GCP_ZONE="us"
 export GOOGLE_APPLICATION_CREDENTIALS="/secrets/data-service-account.json"
 
 
@@ -20,7 +21,7 @@ docker run --rm --name data-version-cli -ti \
 --device /dev/fuse \
 -v "$BASE_DIR":/app \
 -v "$SECRETS_DIR":/secrets \
--v ~/.gitconfig:/etc/gitconfig \
+-v /mnt/c/Users/yuxin/.gitconfig:/etc/gitconfig \
 -e GOOGLE_APPLICATION_CREDENTIALS=$GOOGLE_APPLICATION_CREDENTIALS \
 -e GCP_PROJECT=$GCP_PROJECT \
 -e GCP_ZONE=$GCP_ZONE \
